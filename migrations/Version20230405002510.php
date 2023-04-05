ba08281e0101315c67cc3c4f6d6fed82b3bcdb9a
Flo <florent.sor@epitech.eu>
2023-04-05 02:31:00 +0200

    Version

--- migrations/Version20230405002510.php
+++ migrations/Version20230405002510.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230404215833 extends AbstractMigration
+final class Version20230405002510 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,9 +20,10 @@ final class Version20230404215833 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395');
-        $this->addSql('DROP INDEX IDX_F5299398A76ED395 ON `order`');
-        $this->addSql('ALTER TABLE `order` DROP user_id');
+        $this->addSql('ALTER TABLE `order` ADD user_id INT NOT NULL, CHANGE total_price total_price INT NOT NULL');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
+        $this->addSql('CREATE INDEX IDX_F5299398A76ED395 ON `order` (user_id)');
+        $this->addSql('ALTER TABLE product ADD user_id INT NOT NULL, CHANGE price price INT NOT NULL');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04ADA76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_D34A04ADA76ED395 ON product (user_id)');
     }
@@ -30,10 +31,11 @@ final class Version20230404215833 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE `order` ADD user_id INT NOT NULL');
-        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_F5299398A76ED395 ON `order` (user_id)');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395');
+        $this->addSql('DROP INDEX IDX_F5299398A76ED395 ON `order`');
+        $this->addSql('ALTER TABLE `order` DROP user_id, CHANGE total_price total_price DOUBLE PRECISION NOT NULL');
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04ADA76ED395');
         $this->addSql('DROP INDEX UNIQ_D34A04ADA76ED395 ON product');
+        $this->addSql('ALTER TABLE product DROP user_id, CHANGE price price DOUBLE PRECISION NOT NULL');
     }
 }
