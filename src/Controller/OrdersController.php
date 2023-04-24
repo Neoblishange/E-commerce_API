cbef81e56ca6eacd276eba195d7bb43f6c2fc827
Flo <florent.sor@epitech.eu>
2023-04-24 12:39:28 +0200

    FIX : quantity of products to order

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -6,6 +6,8 @@ use App\Entity\Order;
 use App\Repository\OrderRepository;
 use App\Repository\ProductRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,29 +18,49 @@ class OrdersController extends AbstractController {
     private ProductRepository $productRepository;
     private UserRepository $userRepository;
     private AuthController $authController;
+    private EntityManagerInterface $entityManager;
 
     public function __construct(
         OrderRepository $orderRepository, ProductRepository $productRepository,
-        UserRepository $userRepository, AuthController $authController)
+        UserRepository $userRepository, AuthController $authController,
+        EntityManagerInterface $entityManager)
     {
         $this->orderRepository = $orderRepository;
         $this->productRepository = $productRepository;
         $this->userRepository = $userRepository;
         $this->authController = $authController;
+        $this->entityManager = $entityManager;
     }
 
     public function getAllOrdersOfUser(Request $request): JsonResponse
     {
         if($this->authController->authenticate($request)) {
-            $allOrders = $this->orderRepository->findAll();
-            $response = [
-                'found' => sizeof($allOrders),
-            ];
-            foreach ($allOrders as $order) {
-                $response['orders'][] = $order instanceof Order ? json_decode($order->toJson()->getContent()) : [];
+            try {
+                $allOrders = $this->orderRepository->findAll();
+                $response = [
+                    'found' => sizeof($allOrders),
+                ];
+                $conn = $this->entityManager->getConnection();
+                foreach ($allOrders as $order) {
+                    $response['orders'][] = $order instanceof Order ? json_decode($order->toJson()->getContent()) : [];
+                    $orderId = $order->getId();
+                    $sql = 'SELECT product_id, quantity FROM e_commerce.order_product WHERE order_id = ' . $orderId;
+                    $exec = $conn->fetchAllAssociative($sql);
+                    $products = $response['orders'][array_search($order, $allOrders)]->products;
+                    foreach($products as $product) {
+                        $id = array_search($product, $products);
+                        if($product->id === $exec[$id]["product_id"]) {
+                            $product->quantity = $exec[$id]["quantity"];
+                        }
+                    }
+                }
+                $conn->close();
+                $response = json_encode($response);
+                return new JsonResponse($response, 200, [], true);
             }
-            $response = json_encode($response);
-            return new JsonResponse($response, 200, [], true);
+            catch (Exception $exception) {
+                return new JsonResponse(['error' => "ERROR 404 - Order not found" .  $exception], Response::HTTP_NOT_FOUND, [], false);
+            }
         }
         return new JsonResponse(['error' => "CODE 401 - Unauthorized"], Response::HTTP_UNAUTHORIZED, [], false);
     }
@@ -48,10 +70,25 @@ class OrdersController extends AbstractController {
         if($this->authController->authenticate($request)) {
             $user = $this->authController->getApiToken($request)->getUserId();
             $order = $this->orderRepository->findOneBy(['id' => $orderId, 'user' => $user]);
-            if($order){
-                return new JsonResponse($order instanceof Order ? $order->toJson()->getContent() : [], 200, [], true);
+            try {
+                $conn = $this->entityManager->getConnection();
+                if($order){
+                    $sql = 'SELECT product_id, quantity FROM e_commerce.order_product WHERE order_id = ' . $order->getId();
+                    $exec = $conn->fetchAllAssociative($sql);
+                    $response = $order instanceof Order ? json_decode($order->toJson()->getContent()) : [];
+                    $products = $response->products;
+                    foreach ($products as $product) {
+                        $id = array_search($product, $products);
+                        $response->products[$id]->quantity = $exec[$id]["quantity"];
+                    }
+                    $conn->close();
+                    $response = json_encode($response);
+                    return new JsonResponse($response, 200, [], true);
+                }
             }
-            return new JsonResponse(['error' => "ERROR 404 - Order not found"], Response::HTTP_NOT_FOUND, [], false);
+            catch (Exception $exception) {
+                return new JsonResponse(['error' => "ERROR 404 - Order not found"], Response::HTTP_NOT_FOUND, [], false);
+            }
         }
         return new JsonResponse(['error' => "CODE 401 - Unauthorized"], Response::HTTP_UNAUTHORIZED, [], false);
     }
